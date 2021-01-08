050c168b2ab1cd6643b2b070c573e42ad1e88f5d
Eric Johnson <ericjohnson0216@icloud.com>
2021-01-07 20:41:09 -0500

    Nav

---
+++ client/src/components/Foot/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+    function Foot() {
+        return (
+            
+        <nav className="navbar navbar-expand-lg navbar-primary bg-light" sticky="bottom" >
+            <h1 className="navbar-brand" href="/"> Date Night </h1>
+            <button variant="dark"> Saved </button>
+            <button variant="dark bg-large"> Profile </button>
+        </nav>
+  );
+}
+
+export default Foot;
