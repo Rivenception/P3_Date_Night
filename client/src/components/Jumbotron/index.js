f8ec6dc3e05ff05ab8726e75f78693cad0ac3da0
Eric Johnson <ericjohnson0216@icloud.com>
2021-01-08 23:18:26 -0500

    jumbotron

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Jumbotron({ children }) {
+  return (
+    <div style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }} className="jumbotron">
+        <h1> Date Night </h1>
+				<p>Search for your next date !</p>
+    </div>
+  );
+}
+
+export default Jumbotron;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Nav from "../components/Nav";
-
+import Jumbotron from "../components/Jumbotron";
     class Home extends Component {
     
         state = {
@@ -12,6 +12,7 @@ import Nav from "../components/Nav";
             return (
                 <div>
                     <Nav />
+                    <Jumbotron />
                     <h2>Home Page</h2>
                 </div>
             );
