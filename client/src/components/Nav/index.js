60e4f02b2bba43030f9d6242074e0e1d5a746904
Eric Johnson <ericjohnson0216@icloud.com>
2021-01-08 23:08:30 -0500

    navLinks

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,8 +6,8 @@ import { Link } from "react-router-dom";
             
         <nav className="navbar navbar-expand-lg navbar-primary bg-light" fixed="top" >
             <h1 className="navbar-brand" href="/"> Date Night </h1>
-            <button variant="dark"> Saved </button>
-            <button variant="dark bg-large"> Profile </button>
+            <button> <Link style={styles.link} to="/profile"> Profile </Link> </button>
+            <button> <Link style={styles.link} to="/"> Home </Link> </button>
         </nav>
   );
 }
@@ -16,7 +16,7 @@ export default Nav;
 
     const styles = {
         link: {
-            color: "white",
+            color: "black",
             padding: "20px",
         }
     }
