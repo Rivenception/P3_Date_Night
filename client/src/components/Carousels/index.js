2dbcbc717bcc6d79968f2d17077bd9ac366bbee2
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2021-01-12 23:48:59 -0500

    carousel component added

---
+++ client/src/components/Carousels/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Carousel from 'react-bootstrap/Carousel'
+import image from "../../assets/images/image1.jpg"
+import image2 from "../../assets/images/image2.jpg"
+import image3 from "../../assets/images/image3.jpg"
+
+
+function Carousels(){
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={image}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={image2}
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={image3}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    )
+}
+
+export default Carousels;
--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -1,11 +1,13 @@
 // Github repo for potential use
 // https://github.com/xiaolin/react-image-gallery
-
+import Carousels from "../components/Carousels"
 function Gallery() {
   return (
     <div>
       <br />
       <h2 className="fancy">Gallery</h2>
+      <br></br>
+      <Carousels/>
     </div>
   );
 }
