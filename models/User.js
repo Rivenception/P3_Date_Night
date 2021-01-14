847822808325d9a2617ff029529bbb049c1a44ed
David <davidbrown3142@gmail.com>
2021-01-13 21:16:56 -0500

    user to sequelize (non functional)

--- models/User.js
+++ models/User.js
@@ -1,34 +1,27 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
-mongoose.Promise = Promise;
-
-const userSchema = new Schema({
-    email: { type: String, unique: true, required: true},
-    userName: { type: String, unique: false, required: true},
-    password: { type: String, unique: false, required: true},
-    date: { type: Date, default: Date.now }
-})
-
-userSchema.methods = {
-    checkPassword: function(input){
-        return bcrypt.compareSync(input, this.password)
+module.exports = function(sequelize, DataTypes) {
+    const User = sequelize.define("User", {
+    //   location: {
+    //     allowNull: false,
+    //     type: DataTypes.STRING
+    //   },
+    //   instructions: {
+    //     allowNull: false,
+    //     type: DataTypes.STRING(5000)
+    //   },
+    //   budget: {
+    //     type: DataTypes.STRING,
+    //   },
+    //   items: {
+    //     type: DataTypes.STRING,
+    //   },
+    //   links: {
+    //     type: DataTypes.STRING,
+    //   }
     },
-    hashPassword: plainTextPassword => {
-        return bcrypt.hashSync(plainTextPassword, 10)
-    }
-}
-
-userSchema.pre('save', function(next) {
-    if(!this.password) {
-        console.log('models/user.js ======== NO PASSWORD PROVIDED ========')
-        next();
-    }else {
-        console.log('model/user.js hashPassword in pre save');
-        this.password = this.hashPassword(this.password)
-        next();
-    }
-})
-
-const User = moongoose.model("User", userSchema);
-module.exports = User
+    {
+      timestamps: false
+    });
+    return User;
+  };
+  
+  
