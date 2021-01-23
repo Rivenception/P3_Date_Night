8a1777a6af179bae008ac17630d41f089bd2f27b
Eric Johnson <ericjohnson0216@icloud.com>
2021-01-23 13:36:43 -0500

    commit14

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = require("./routes");
 const csv = require('csvtojson');
 const db = require("./models");
 var fs = require("fs");
-// const path = require("path");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -17,14 +17,13 @@ if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 }
 
-// app.get('*',(req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// });
-
 // Add routes, both API and view
 app.use(routes);
 // app.use(require("./routes/index.js"));
 
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 // Start the API server
 db.sequelize.sync().then(function(){
 	app.listen(PORT, function () {
