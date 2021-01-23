8682af044b5c9b5d311e52cfcb8fdcc18ded7942
Eric Johnson <ericjohnson0216@icloud.com>
2021-01-23 12:57:23 -0500

    commit12

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 }
+
 // Add routes, both API and view
 app.use(routes);
 // app.use(require("./routes/index.js"));
